7c17d10fe0afb3f915cddc829013b7b0676a5b70
Xime182026 <ximguzman16@gmail.com>
2026-03-23 20:36:24 -0500

    objetos

---
+++ clases_perro.py
@@ -0,0 +1,19 @@
+#clases 
+class perro:
+    def _init_(self, nombre, edad): #funciones
+        self.nombre  = nombre #
+        self.nombre = edad
+        
+#class 
+
+        
+        
+class Perro:
+    def ladrar(self):
+        return "¡Guau!"
+    def rascarse(self):
+        print("Me estoy rascando" )
+        
+perro1 = perro("roki", 5)
+perrito = Perro("firulais" .3)
+print(perro1.nombre, perro1.edad)
